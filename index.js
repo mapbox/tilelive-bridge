9ede75f628fa057c855f261c982134d5155715ba
Young Hahn <young@developmentseed.org>
2013-12-04 14:15:30 -0500

    Additional SRSs for 900913.

--- index.js
+++ index.js
@@ -214,6 +214,7 @@ Bridge.prototype.getIndexableDocs = function(pointer, callback) {
     pointer.offset = pointer.offset || 0;
 
     var knownsrs = {
+        '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over': '900913',
         '+proj=merc +lon_0=0 +lat_ts=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs': '900913',
         '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs': 'WGS84'
     };
