16823ba53600b70c03c6679b0388f3479a3e5dfa
Young Hahn <young@developmentseed.org>
2013-12-10 01:54:23 -0500

    Release map back to pool when indexing.

--- index.js
+++ index.js
@@ -210,16 +210,20 @@ Bridge.prototype.getIndexableDocs = function(pointer, callback) {
     pointer.limit = pointer.limit || 10000;
     pointer.offset = pointer.offset || 0;
 
+    var source = this;
     var knownsrs = {
         '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over': '900913',
         '+proj=merc +lon_0=0 +lat_ts=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs': '900913',
         '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs': 'WGS84'
     };
 
-    this.getInfo(function(err, info) {
+    source.getInfo(function(err, info) {
         if (err) return callback(err);
         if (!info.maxzoom) return callback(new Error('No maxzoom defined'));
-        this._map.acquire(function(err, map) {
+        source._map.acquire(function(err, map) {
+            if (err) return callback(err);
+            process.nextTick(function() { source._map.release(map) });
+
             var name = (map.parameters.geocoder_layer||'').split('.').shift() || '';
             var field = (map.parameters.geocoder_layer||'').split('.').pop() || '_text';
             var layer = name
@@ -291,7 +295,7 @@ Bridge.prototype.getIndexableDocs = function(pointer, callback) {
                 tiles();
             }
             feature();
-        }.bind(this));
-    }.bind(this));
+        });
+    });
 };
 
